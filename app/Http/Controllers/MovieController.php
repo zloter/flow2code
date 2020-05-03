9dbcf61e35c8ea62d27aaa0895c14c205c7802c1
Zygmunt Loter <zygmuntloter@gmail.com>
2020-05-03 22:44:44 +0000

    Working movie add (to be refactored)

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,7 +4,12 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\MovieRequest;
+use App\Models\Country;
+use App\Models\Genre;
+use App\Models\Image;
 use App\Models\Movie;
+use Illuminate\Support\Str;
 
 class MovieController extends Controller
 {
@@ -25,4 +30,46 @@ class MovieController extends Controller
         });
         return response()->json($movies);
     }
+
+    public function add(MovieRequest $request) {
+        $fileName = Str::random(20) . ' ' . $request->file('image')->getClientOriginalName();
+        $request->file('image')->storeAs('/movies/posters/', $fileName);
+        $image = new Image(['path' => '/movies/posters/' . $fileName]);
+        $image->save();
+        $movie = new Movie($request->all());
+        $movie->image_id = $image->id;
+        $movie->save();
+        $genres = json_decode($request->get('genres'));
+        $movie->genres()->attach($genres);
+        return response()->json($request->all());
+    }
+
+    public function update(Movie $movie) {
+
+    }
+
+    public function delete(Movie $movie) {
+
+    }
+
+    /**
+     * Return list of available genres & countries for movie
+     */
+    public function data() {
+        return response()->json([
+            'genres' => Genre::select('id', 'name')->get(),
+            'countries' => Country::select('id', 'name')->get()
+        ]);
+    }
+
+//    /**
+//     * Return list of available genres & countries for movie
+//     */
+//    public function search($item) {
+//        $movies = Movie::with(['genres', 'country', 'image'])
+//            ->where('title', 'LIKE', "%{$item}%")
+//            ->orWhere('description', 'LIKE', "%{$item}%")
+//            ->get();
+//        return response()->json($movies);
+//    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/movies', 'MovieController@list')->name('list');
+Route::get('/movies', 'MovieController@list')->name('movies.list');
+Route::get('/movies/data', 'MovieController@data')->name('movies.data');
+Route::get('/movies/search/{item}', 'MovieController@search')->name('movies.search');
+Route::post('/movies', 'MovieController@add')->name('movies.add');;
+Route::put('/movies/{movie}', 'MovieController@update')->name('movies.update');
+Route::delete('/movies/{movie}', 'MovieController@update')->name('movies.update');
 
