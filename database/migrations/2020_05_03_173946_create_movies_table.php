c8818fe8a4ef9fa5a22f7db55fa06aa3b9699895
Zygmunt Loter <zygmuntloter@gmail.com>
2020-05-03 18:00:51 +0000

    Database basic structure

---
+++ database/migrations/2020_05_03_173946_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('image_id')->constrained();
+            $table->text('description');
+            $table->foreignId('country_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
